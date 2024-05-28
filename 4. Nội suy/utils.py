c3d6837048f0cd7873d36e2d76ef0df39110ca90
Minh Quang Bui <minhquangbui053@gmail.com>
2024-05-28 23:34:14 +0700

    finish 4

--- 4. Nội suy/utils.py
+++ 4. Nội suy/utils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot(points=None, func=None):
+
+def plot_points_and_func(points=None, func=None):
     """
     Plot points and/or a given function over the x-axis range determined by the points.
 
@@ -15,7 +16,7 @@ def plot(points=None, func=None):
     if points:
         x_values = [point[0] for point in points]
         y_values = [point[1] for point in points]
-        plt.plot(x_values, y_values, 'b-', label='Points')  # Use 'b-' for blue line
+        plt.plot(x_values, y_values, "b-", label="Points")  # Use 'b-' for blue line
 
         # Determine x-axis range based on points
         x_min = min(x_values)
@@ -28,12 +29,64 @@ def plot(points=None, func=None):
             x_range = (-10, 10)  # Default x-axis range
         x_values = np.linspace(x_range[0], x_range[1], 400)
         y_values = func(x_values)
-        plt.plot(x_values, y_values, 'r-', label=f'{func.__name__}(x)')  # Use 'r-' for red line
+        plt.plot(
+            x_values, y_values, "r-", label=f"{func.__name__}(x)"
+        )  # Use 'r-' for red line
 
     # Add title and labels
-    plt.title('Plot')
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.title("Plot")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    # Add a legend
+    plt.legend()
+
+    # Show grid
+    plt.grid(True)
+
+    # Display the plot
+    plt.show()
+
+
+def plot_points_and_points(points_1=None, points_2=None):
+    """
+    Plot points and/or a given function over the x-axis range determined by the points.
+
+    Parameters:
+    points (list of tuples): List of (x, y) points to be plotted.
+    func (function): The function to plot.
+    """
+    plt.figure(figsize=(8, 6))
+
+    # Plot points if provided
+    if points_1:
+        x_values = [point[0] for point in points_1]
+        y_values = [point[1] for point in points_1]
+        plt.plot(
+            x_values, y_values, "b-", label="Data points"
+        )  # Use 'b-' for blue line
+
+        # Determine x-axis range based on points
+        x_min = min(x_values)
+        x_max = max(x_values)
+        x_range = (x_min, x_max)
+
+    if points_2:
+        x_values = [point[0] for point in points_2]
+        y_values = [point[1] for point in points_2]
+        plt.plot(
+            x_values, y_values, "r-", label="Estimated points"
+        )  # Use 'b-' for blue line
+
+        # Determine x-axis range based on points
+        x_min = min(x_values)
+        x_max = max(x_values)
+        x_range = (x_min, x_max)
+
+    # Add title and labels
+    plt.title("Plot")
+    plt.xlabel("x")
+    plt.ylabel("y")
 
     # Add a legend
     plt.legend()
